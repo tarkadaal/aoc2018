dfdbfbf191ddb4450939fd50a7f1276668015b3d
Dan Jackson <danjacksonis@googlemail.com>
2018-12-02 17:14:58 +0100

    Draft solution for day 1 part 1

--- day1/main.py
+++ day1/main.py
@@ -1,4 +1,40 @@
 #!/usr/bin/python3
+from sys import argv
 
 
-print('hello world!')
+tests = [
+    ([+1, -2, +3, +1], 3),
+    ([+1, +1, +1], 3),
+    ([+1, +1, -2], 0),
+    ([-1, -2, -3], -6)
+]
+
+def parse(filename):
+    file_data = open(filename).read()
+    return [int(x) for x in file_data.split()]
+
+def solve(data):
+    result = 0
+    for number in data:
+        result = result + number
+    return result
+
+def run_test(test):
+    return solve(test[0]) == test[1]
+
+def run_all_tests():
+    return all([run_test(x) for x in tests])
+
+def main(filename):
+    print('Day 1 exercise 1!')
+    if run_all_tests():
+        data = parse(filename)
+        print("Result is: {}".format(solve(data)))
+    else:
+        print('We be fucked')
+
+if __name__ == '__main__':
+    name = argv[1]
+    filename = 'input_' + name + '.txt'
+    main(filename)
+
