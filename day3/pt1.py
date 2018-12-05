8bc649633cd5ffd0911b08ab75f9c65603a97cf9
Dan Jackson <danjacksonis@googlemail.com>
2018-12-05 20:28:06 +0100

    Added teh pythonz

--- day3/pt1.py
+++ day3/pt1.py
@@ -10,7 +10,7 @@ from sys import argv
 from collections import namedtuple
 import re
 
-Claim = namedtuple('Claim', ['id', 'x', 'y', 'xlength', 'ylength'])
+Claim = namedtuple('Claim', ['id', 'xpos', 'ypos', 'xlength', 'ylength'])
 
 sample_data = [
     '#1 @ 1,3: 4x4',
@@ -23,11 +23,10 @@ expected_result = 4
 def parse_line(line):
     '''
     >>> parse_line('#1 @ 1,3: 4x7')
-    Claim(id=1, x=1, y=3, xlength=4, ylength=7)
+    Claim(id=1, xpos=1, ypos=3, xlength=4, ylength=7)
     >>> parse_line('#150 @ 90,952: 15x18')
-    Claim(id=150, x=90, y=952, xlength=15, ylength=18)
+    Claim(id=150, xpos=90, ypos=952, xlength=15, ylength=18)
     '''
-    print(line)
     id_string = re.findall(r'#\d+', line)[0]
     id = int(re.findall(r'\d+', id_string)[0])
     xy_string = re.findall(r'\d+,\d+', line)[0]
@@ -43,29 +42,28 @@ def parse(filename):
 
 
 def calculate_coords(xpos, ypos, xlength, ylength, coord_dict):
-    
-
     for j in range(1, ylength+1):
         for i in range(1, xlength+1):
             coord = (xpos+i, ypos+j)
-            if coord in coord_dict:
-                coord_dict[coord] += 1
-            else:
-                coord_dict[coord] = 1
+            if coord not in coord_dict:
+                coord_dict[coord] = 0
+            coord_dict[coord] += 1
 
-    return
 
 def solve(data):
     coord_dict = dict()
-    total_collision = 0
     for line in data:
-        calculate_coords(line.x, line.y, line.xlength, line.ylength, coord_dict)
+        calculate_coords(line.xpos, line.ypos, line.xlength, line.ylength, coord_dict)
     
     # count all entries in dict with value of 2 or more
-    for line in coord_dict:
-        if coord_dict[line] > 1:
-            total_collision += 1
-    return total_collision
+    # equivalent to:
+    # total_collision = 0
+    # for line in coord_dict:
+    #     if coord_dict[line] > 1:
+    #         total_collision += 1
+    # return total_collision
+    return sum([x > 1 for x in coord_dict.values()])
+
 
 def run_test():
     return solve([parse_line(x) for x in sample_data]) == expected_result
