028a9a7a06388dfe17099adf8debdadd8a23d008
Dan Jackson <danjacksonis@googlemail.com>
2018-12-05 19:25:29 +0100

    File/string parsing code for day3p1

--- day3/pt1.py
+++ day3/pt1.py
@@ -7,6 +7,10 @@
 
 
 from sys import argv
+from collections import namedtuple
+import re
+
+Claim = namedtuple('Claim', ['id', 'x', 'y', 'width', 'height'])
 
 tests = [
     '#1 @ 1,3: 4x4',
@@ -16,18 +20,25 @@ tests = [
 
 answer = 4
 
-
 def parse_line(line):
     '''
-    >>> parse_line('#1 @ 1,3: 4x4')
-    
+    >>> parse_line('#1 @ 1,3: 4x7')
+    Claim(id=1, x=1, y=3, width=4, height=7)
+    >>> parse_line('#150 @ 90,952: 15x18')
+    Claim(id=150, x=90, y=952, width=15, height=18)
     '''
-    pass
+    id_string = re.findall(r'#\d+', line)[0]
+    id = int(re.findall(r'\d+', id_string)[0])
+    xy_string = re.findall(r'\d+,\d+', line)[0]
+    x,y= [int(x) for x in xy_string.split(',')]
+    wh_string = re.findall(r'\d+x\d+', line)[0]
+    w, h = [int(x) for x in wh_string.split('x')]
+    return Claim(id,x,y,w,h)
 
 
 def parse(filename):
     file_data = open(filename).read()
-    return [x for x in file_data.split()]
+    return [parse_line(x) for x in file_data.split()]
 
 
 # def solve_one(first, second):
